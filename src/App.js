761b9268db95a87e8f2bde933b55001d35516733
eltoncampos1 <qazwsx272518>
2020-07-01 18:37:21 -0300

    Conceitos ReactJS Desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [ repositories,setRepositories] = useState([])
+    useEffect(() => {
+      api.get('repositories').then(response =>{
+        setRepositories(response.data)
+      })
+    },[])
+  
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title:'Conceitos reactJS',
+      url:'https://github.com/eltoncampos1/gostack-desafio-conceitos-reactJs',
+      techs:['Node.JS', 'ReactJs']
+    })
+
+    setRepositories([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
